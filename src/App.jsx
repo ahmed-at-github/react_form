f58fca4275cdf752b04090fb9ecbcabdbe58d9e3
ahmed-at-github <ahmedhussainnow@gmail.com>
2025-06-05 21:51:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import FormComp from './components/FormComp'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World! Iam App</h1>
+      <FormComp />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/FormComp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FormComp() {
+    return (
+        <div>Hello World! Iam FormComp</div>
+    )
+}
